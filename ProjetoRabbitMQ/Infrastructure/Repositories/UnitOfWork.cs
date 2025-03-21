cc581bc6344f0a4f1497eec6bcf1987015dc460c
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-03-21 00:11:20 -0300

    feat: added Repository for MangaOrderItem

--- ProjetoRabbitMQ/Infrastructure/Repositories/UnitOfWork.cs
+++ ProjetoRabbitMQ/Infrastructure/Repositories/UnitOfWork.cs
@@ -7,13 +7,22 @@ namespace ProjetoRabbitMQ.Infrastructure.Repositories
 {
     public class UnitOfWork(MySqlContext context) : IUnitOfWork, IDisposable
     {
-        private readonly Lazy<IRepository<UserEntity>> _userRepository = new(() => new Repository<UserEntity>(context));
-        private readonly Lazy<IRepository<MangaEntity>> _mangaRepository = new(() => new Repository<MangaEntity>(context));
-        private readonly Lazy<IRepository<MangaOrderEntity>> _mangaOrderRepository = new(() => new Repository<MangaOrderEntity>(context));
+        private readonly Lazy<IRepository<UserEntity>> _userRepository 
+            = new(() => new Repository<UserEntity>(context));
+        
+        private readonly Lazy<IRepository<MangaEntity>> _mangaRepository 
+            = new(() => new Repository<MangaEntity>(context));
+        
+        private readonly Lazy<IRepository<MangaOrderEntity>> _mangaOrderRepository 
+            = new(() => new Repository<MangaOrderEntity>(context));
+        
+        private readonly Lazy<IRepository<MangaOrderItemEntity>> _mangaOrderItemRepository 
+            = new(() => new Repository<MangaOrderItemEntity>(context));
 
         public IRepository<UserEntity> UserRepository => _userRepository.Value;
         public IRepository<MangaEntity> MangaRepository => _mangaRepository.Value;
         public IRepository<MangaOrderEntity> MangaOrderRepository => _mangaOrderRepository.Value;
+        public IRepository<MangaOrderItemEntity> MangaOrderItemRepository => _mangaOrderItemRepository.Value;
 
         public async Task CommitAsync()
         {
