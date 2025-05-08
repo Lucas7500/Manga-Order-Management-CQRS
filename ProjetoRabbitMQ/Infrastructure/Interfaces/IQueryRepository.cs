1ffb3cc99da5793afab3e317f03f2cfb4bb8fdfd
Lucas lglesias <lucas.lglesias@inventsoftware.com.br>
2025-05-08 10:37:38 -0300

    refactor: changed name of cancellation token

--- ProjetoRabbitMQ/Infrastructure/Interfaces/IQueryRepository.cs
+++ ProjetoRabbitMQ/Infrastructure/Interfaces/IQueryRepository.cs
@@ -4,9 +4,9 @@ namespace ProjetoRabbitMQ.Infrastructure.Interfaces
 {
     public interface IQueryRepository<TEntity> where TEntity : class
     {
-        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken ct = default);
-        Task<TEntity?> GetAsync(object[] id, CancellationToken ct = default);
-        Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default);
-        Task<bool> HasAnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default);
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default);
+        Task<TEntity?> GetAsync(object[] id, CancellationToken cancellationToken = default);
+        Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<bool> HasAnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
     }
 }
--- ProjetoRabbitMQ/Infrastructure/Repositories/Repository.cs
+++ ProjetoRabbitMQ/Infrastructure/Repositories/Repository.cs
@@ -12,14 +12,14 @@ namespace ProjetoRabbitMQ.Infrastructure.Repositories
 
         public DbSet<TEntity> DbSet => _dbSet;
 
-        public Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken ct = default)
+        public Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default)
         {
             var query = predicate != null ? _dbSet.Where(predicate) : _dbSet;
             DefineIncludesForQuery(ref query);
-            return query.AsNoTracking().ToListAsync(ct);
+            return query.AsNoTracking().ToListAsync(cancellationToken);
         }
 
-        public async Task<TEntity?> GetAsync(object[] id, CancellationToken ct = default)
+        public async Task<TEntity?> GetAsync(object[] id, CancellationToken cancellationToken = default)
         {
             if (_includes.Count != 0)
             {
@@ -27,24 +27,24 @@ namespace ProjetoRabbitMQ.Infrastructure.Repositories
                 throw new NotSupportedException("Includes are not supported for GetAsync with id.");
             }
 
-            return await _dbSet.FindAsync(id, ct);
+            return await _dbSet.FindAsync(id, cancellationToken);
         }
 
-        public Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default)
+        public Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
         {
             var query = _dbSet.Where(predicate);
             DefineIncludesForQuery(ref query);
-            return query.FirstOrDefaultAsync(ct);
+            return query.FirstOrDefaultAsync(cancellationToken);
         }
 
-        public Task<bool> HasAnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken ct = default)
+        public Task<bool> HasAnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default)
         {
-            return _dbSet.AnyAsync(predicate, ct);
+            return _dbSet.AnyAsync(predicate, cancellationToken);
         }
 
-        public async Task AddAsync(TEntity entity, CancellationToken ct = default)
+        public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
         {
-            await _dbSet.AddAsync(entity, ct);
+            await _dbSet.AddAsync(entity, cancellationToken);
         }
 
         public void Update(TEntity entity)
