fc1dc90f87fa1e5da5a279aade8d25ab9676a8e8
Lucas lglesias <lucas.lglesias@inventsoftware.com.br>
2025-05-08 10:36:27 -0300

    feat: added Mapper Extensions for Users and Mangas

---
+++ ProjetoRabbitMQ/Extensions/Mappers/MangaMapper.cs
@@ -0,0 +1,58 @@
+﻿using ProjetoRabbitMQ.Models.Manga.Commands;
+using ProjetoRabbitMQ.Models.Manga.Responses;
+using ProjetoRabbitMQ.Models.Manga;
+
+namespace ProjetoRabbitMQ.Extensions.Mappers
+{
+    public static class MangaMapper
+    {
+        public static IEnumerable<MangaQueryModel> ToQueryModel(this IEnumerable<MangaEntity> mangas)
+        {
+            return mangas.Select(manga => manga.ToQueryModel());
+        }
+
+        public static MangaQueryModel ToQueryModel(this MangaEntity manga)
+        {
+            return new MangaQueryModel(
+                manga.Title,
+                manga.Author,
+                manga.ReleaseDate,
+                manga.Genres,
+                manga.Aliases,
+                manga.Quantity,
+                manga.Price,
+                manga.Description);
+        }
+
+        public static MangaEntity ToEntity(this MangaQueryModel queryModel)
+        {
+            return new MangaEntity
+            {
+                Title = queryModel.Title,
+                Author = queryModel.Author,
+                ReleaseDate = queryModel.ReleaseDate,
+                Quantity = queryModel.Quantity,
+                Price = queryModel.Price,
+                Description = queryModel.Description,
+                Genres = queryModel.Genres.ToList(),
+                Aliases = queryModel.Aliases.ToList()
+            };
+        }
+
+        public static MangaEntity ToEntity(this CreateMangaCommand command)
+        {
+            return new MangaEntity
+            {
+                Id = Guid.NewGuid(),
+                Title = command.Title,
+                Author = command.Author,
+                ReleaseDate = command.ReleaseDate,
+                Quantity = command.Quantity,
+                Price = command.Price,
+                Description = command.Description,
+                Genres = command.Genres.ToList(),
+                Aliases = command.Aliases.ToList()
+            };
+        }
+    }
+}
---
+++ ProjetoRabbitMQ/Extensions/Mappers/UserMapper.cs
@@ -0,0 +1,34 @@
+﻿using ProjetoRabbitMQ.Models.User;
+using ProjetoRabbitMQ.Models.User.Commands;
+using ProjetoRabbitMQ.Models.User.Responses;
+
+namespace ProjetoRabbitMQ.Extensions.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserEntity ToEntity(this CreateUserCommand command, string hashedPassword)
+        {
+            return new UserEntity
+            {
+                Name = command.Name,
+                Email = command.Email,
+                PasswordHash = hashedPassword,
+                Role = command.Role
+            };
+        }
+
+        public static IEnumerable<UserQueryModel> ToQueryModel(this IEnumerable<UserEntity> users)
+        {
+            return users.Select(user => user.ToQueryModel());
+        }
+
+        public static UserQueryModel ToQueryModel(this UserEntity user)
+        {
+            return new UserQueryModel(
+                user.Id,
+                user.Name,
+                user.Email,
+                user.Role);
+        }
+    }
+}
