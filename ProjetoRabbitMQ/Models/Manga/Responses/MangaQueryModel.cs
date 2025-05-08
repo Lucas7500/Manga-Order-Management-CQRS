2b457e9cef675f4d98bbc9da932164f90e4fb47a
Lucas lglesias <lucas.lglesias@inventsoftware.com.br>
2025-05-08 10:39:22 -0300

    feat: creating models to return in my get endpoints of Users and Mangas

---
+++ ProjetoRabbitMQ/Models/Manga/Responses/MangaQueryModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoRabbitMQ.Models.Manga.Responses
+{
+    public record MangaQueryModel(
+            string Title,
+            string Author,
+            DateTime ReleaseDate,
+            IEnumerable<string> Genres,
+            IEnumerable<string> Aliases,
+            uint Quantity,
+            decimal Price,
+            string? Description
+        );
+}
---
+++ ProjetoRabbitMQ/Models/User/Responses/UserQueryModel.cs
@@ -0,0 +1,11 @@
+﻿using ProjetoRabbitMQ.Models.Enums;
+
+namespace ProjetoRabbitMQ.Models.User.Responses
+{
+    public record UserQueryModel(
+        int Id,
+        string Name,
+        string Email,
+        UserRole Role
+    );
+}
